364437d076ad9bd4b1f7e0f8fd270c7536693234
David Fernando Torres Zapata <fernando.zapata.live@gmail.com>
2022-08-25 16:06:45 -0500

    Fixed error in html home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,9 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	@vite('resources/js/app.js')
 	<title>Document</title>
 </head>
 <body>
-
-	<main id="app">
-		<h1>Hola</h1>
-		<index/>
-	</main>
-
+	<h1>Hola</h1>
 </body>
 </html>
